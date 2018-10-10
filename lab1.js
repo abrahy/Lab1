7b85871d116e41ecc9d6084e8953d823c74be02e
Brahy <braands@dunwoody.edu>
2018-10-09 19:19:46 -0500

    lab1.js

---
+++ lab1.js
@@ -0,0 +1,290 @@
+var data = [
+  {
+    "Name": "Miami Dolphins",
+    "Record": "six-ten",
+    "Offense": 28,
+    "Defense": 29,
+    "Conference": "AFC East",
+    "Wins": 6,
+    "Losses": 10
+  },
+  {
+    "Name": "Buffalo Bills",
+    "Record": "nine-seven",
+    "Offense": 22,
+    "Defense": 18,
+    "Conference": "AFC East",
+    "Wins": 9,
+    "Losses": 7
+  },
+  {
+    "Name": "New York Jets",
+    "Record": "five-eleven",
+    "Offense": 24,
+    "Defense": 23,
+    "Conference": "AFC East",
+    "Wins": 5,
+    "Losses": 11
+  },
+  {
+    "Name": "New England Patriots",
+    "Record": "thirteen-three",
+    "Offense": 2,
+    "Defense": 5,
+    "Conference": "AFC East",
+    "Wins": 13,
+    "Losses": 3
+  },
+  {
+    "Name": "Minnesota Vikings",
+    "Record": "thirteen-three",
+    "Offense": 10,
+    "Defense": 1,
+    "Conference": "NFC North",
+    "Wins": 13,
+    "Losses": 3
+  },
+  {
+    "Name": "Green Bay Packers",
+    "Record": "seven-nine",
+    "Offense": 21,
+    "Defense": 26,
+    "Conference": "NFC North",
+    "Wins": 7,
+    "Losses": 9
+  },
+  {
+    "Name": "Detroit Lions",
+    "Record": "nine-seven",
+    "Offense": 7,
+    "Defense": 21,
+    "Conference": "NFC North",
+    "Wins": 9,
+    "Losses": 7
+  },
+  {
+    "Name": "Chicago Bears",
+    "Record": "five-eleven",
+    "Offense": 29,
+    "Defense": 9,
+    "Conference": "NFC North",
+    "Wins": 5,
+    "Losses": 11
+  },
+  {
+    "Name": "Kansas City Chiefs",
+    "Record": "ten-six",
+    "Offense": 6,
+    "Defense": 15,
+    "Conference": "AFC West",
+    "Wins": 10,
+    "Losses": 6
+  },
+  {
+    "Name": "Denver Broncos",
+    "Record": "five-eleven",
+    "Offense": 27,
+    "Defense": 24,
+    "Conference": "AFC West",
+    "Wins": 5,
+    "Losses": 11
+  },
+  {
+    "Name": "Los Angeles Chargers",
+    "Record": "nine-seven",
+    "Offense": 13,
+    "Defense": 3,
+    "Conference": "AFC West",
+    "Wins": 9,
+    "Losses": 7
+  },
+  {
+    "Name": "Oakland Raiders",
+    "Record": "six-ten",
+    "Offense": 23,
+    "Defense": 20,
+    "Conference": "AFC West",
+    "Wins": 6,
+    "Losses": 10
+  },
+  {
+    "Name": "Atlanta Falcons",
+    "Record": "ten-six",
+    "Offense": 15,
+    "Defense": 8,
+    "Conference": "NFC  South",
+    "Wins": 10,
+    "Losses": 6
+  },
+  {
+    "Name": "New Orleans Saints",
+    "Record": "eleven-five",
+    "Offense": 4,
+    "Defense": 10,
+    "Conference": "NFC South",
+    "Wins": 11,
+    "Losses": 5
+  },
+  {
+    "Name": "Carolina Panthers",
+    "Record": "eleven-five",
+    "Offense": 12,
+    "Defense": 11,
+    "Conference": "NFC South",
+    "Wins": 11,
+    "Losses": 5
+  },
+  {
+    "Name": "Tampa Bay Buccaneers",
+    "Record": "five-eleven",
+    "Offense": 18,
+    "Defense": 22,
+    "Conference": "NFC South",
+    "Wins": 5,
+    "Losses": 11
+  },
+  {
+    "Name": "Dallas Cowboys",
+    "Record": "nine-seven",
+    "Offense": 14,
+    "Defense": 13,
+    "Conference": "NFC East",
+    "Wins": 9,
+    "Losses": 7
+  },
+  {
+    "Name": "Philadelphia Eagles",
+    "Record": "thirteen-three",
+    "Offense": 3,
+    "Defense": 4,
+    "Conference": "NFC East",
+    "Wins": 13,
+    "Losses": 3
+  },
+  {
+    "Name": "New York Giants",
+    "Record": "three-thirteen",
+    "Offense": 31,
+    "Defense": 28,
+    "Conference": "NFC East",
+    "Wins": 3,
+    "Losses": 13
+  },
+  {
+    "Name": "Washington Redskins",
+    "Record": "seven-nine",
+    "Offense": 16,
+    "Defense": 27,
+    "Conference": "NFC East",
+    "Wins": 7,
+    "Losses": 9
+  },
+  {
+    "Name": "Los Angeles Rams",
+    "Record": "eleven-five",
+    "Offense": 1,
+    "Defense": 12,
+    "Conference": "NFC West",
+    "Wins": 11,
+    "Losses": 5
+  },
+  {
+    "Name": "Seattle Seahawks",
+    "Record": "nine-seven",
+    "Offense": 11,
+    "Defense": 14,
+    "Conference": "NFC West",
+    "Wins": 9,
+    "Losses": 7
+  },
+  {
+    "Name": "Arizona Cardnials",
+    "Record": "eight-eight",
+    "Offense": 25,
+    "Defense": 19,
+    "Conference": "NFC West",
+    "Wins": 8,
+    "Losses": 8
+  },
+  {
+    "Name": "San Fransico 49ers",
+    "Record": "six-ten",
+    "Offense": 20,
+    "Defense": 25,
+    "Conference": "NFC West",
+    "Wins": 6,
+    "Losses": 10
+  },
+  {
+    "Name": "Baltimore Ravens",
+    "Record": "nine-seven",
+    "Offense": 9,
+    "Defense": 6,
+    "Conference": "AFC North",
+    "Wins": 9,
+    "Losses": 7
+  },
+  {
+    "Name": "Cinncinatti Bengals",
+    "Record": "seven-nine",
+    "Offense": 26,
+    "Defense": 16,
+    "Conference": "AFC North",
+    "Wins": 7,
+    "Losses": 9
+  },
+  {
+    "Name": "Pittsburgh Steelers",
+    "Record": "thirteen-three",
+    "Offense": 8,
+    "Defense": 7,
+    "Conference": "AFC North",
+    "Wins": 13,
+    "Losses": 3
+  },
+  {
+    "Name": "Cleveland Browns",
+    "Record": "zero-sixteen",
+    "Offense": 32,
+    "Defense": 31,
+    "Conference": "AFC North",
+    "Wins": 0,
+    "Losses": 16
+  },
+  {
+    "Name": "Houston Texans",
+    "Record": "four-twelve",
+    "Offense": 17,
+    "Defense": 32,
+    "Conference": "AFC South",
+    "Wins": 4,
+    "Losses": 12
+  },
+  {
+    "Name": "Indianapolis Colts",
+    "Record": "four-twelve",
+    "Offense": 30,
+    "Defense": 30,
+    "Conference": "AFC South",
+    "Wins": 4,
+    "Losses": 12
+  },
+  {
+    "Name": "Tennesse Titans",
+    "Record": "nine-seven",
+    "Offense": 19,
+    "Defense": 17,
+    "Conference": "AFC South",
+    "Wins": 9,
+    "Losses": 7
+  },
+  {
+    "Name": "Jacksonville Jaguars",
+    "Record": "ten-six",
+    "Offense": 5,
+    "Defense": 2,
+    "Conference": "AFC South",
+    "Wins": 10,
+    "Losses": 6
+  }
+]
